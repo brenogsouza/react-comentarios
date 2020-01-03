5cb6c80ebaafd8a89fee53173c7044003fb3e9c5
brenogsouza <breno.souza.developer@gmail.com>
2020-01-03 14:21:26 -0300

    started application and cleaned skeleton generated by react-app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import React from 'react';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
     <div>
-     <h1>ola mundo</h1>
+      <h1>ola mundo</h1>
     </div>
   );
 }
